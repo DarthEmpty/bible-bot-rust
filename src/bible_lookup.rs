4661fe70eb045a7f1ca3ca9aabf575b3d3d7166e
Joel Clarke <joel.clarke273@gmail.com>
2019-08-20 02:36:07 +0100

    Implement "extract_refs" method

---
+++ src/bible_lookup.rs
@@ -0,0 +1,36 @@
+use regex::Regex;
+
+pub fn extract_refs(text: &str) -> Vec<String> {
+    // Matches with:
+    // <book><chapter> (book may have digit as prefix)
+    // <book><chapter>:<verse>
+    // <book><chapter>:<verse>-<verse>
+    const PATTERN_STRING: &str = r"\[\[(\d?[a-zA-Z]+\d+(?::\d+(?:-\d+)?)?)\]\]";
+    
+    let pattern = Regex::new(PATTERN_STRING).unwrap();
+    let string = text.replace(" ", "").replace("\\", "");
+    let mut refs = Vec::new();
+
+    for cap in pattern.captures_iter(&string) {
+        refs.push(String::from(cap.get(1).unwrap().as_str()));
+    }
+
+    refs
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_extract_refs() {
+        let refs = extract_refs(
+            "I wanna look at [[John 3: 16 - 17]]
+            and I wanna look at [[1Corinthians13]]",
+        );
+
+        assert_eq!(
+            refs,
+            vec!["John3:16-17", "1Corinthians13"]
+        );
+    }
+}
