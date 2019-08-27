ce758f918d56317d11be6a03ed56b95072a1aad4
Joel Clarke <joel.clarke273@gmail.com>
2019-08-27 01:21:29 +0100

    Implement reading bot config from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 mod bible_lookup;
 
+use orca::App;
+use serde::Deserialize;
+use serde_json::{self, Value};
+use std::collections::HashMap;
+use std::fs::read_to_string;
+use std::io;
+use toml;
+
+fn read_config() -> io::Result<String> {
+    const CONFIG_FILE: &str = "src/config.toml";
+    read_to_string(CONFIG_FILE)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let toml_str = read_config().expect("No file found");
+    let config: HashMap<String, String> = toml::from_str(&toml_str).expect("Could not deserialize file");
+    println!("{:?}", config);
 }
