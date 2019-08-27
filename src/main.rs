eb3e46d4733b8051a77172d131453f8e11b56c7b
Joel Clarke <joel.clarke273@gmail.com>
2019-08-27 16:44:50 +0100

    Implement creating reddit connection and retrieving comments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
 mod bible_lookup;
 
-use orca::App;
-use serde::Deserialize;
-use serde_json::{self, Value};
+use orca::{data::Comment, data::Listing, App};
 use std::collections::HashMap;
 use std::fs::read_to_string;
-use std::io;
 use toml;
 
-fn read_config() -> io::Result<String> {
+fn read_config() -> HashMap<String, String> {
     const CONFIG_FILE: &str = "src/config.toml";
-    read_to_string(CONFIG_FILE)
+    let toml = read_to_string(CONFIG_FILE).expect("No file found");
+    toml::from_str(&toml).expect("Could not deserialize file")
 }
 
+fn create_app(config: HashMap<String, String>) -> App {
+    let mut app = App::new(&config["app_name"], &config["version"], &config["author"])
+        .expect("Could not create Reddit instance");
+
+    app
+        .authorize_script(&config["client_id"], &config["client_secret"], &config["username"], &config["password"])
+        .expect("Could not authorize script");
+
+    app
+}
+
+fn get_comments(reddit: &App) -> Listing<Comment> {
+    const SUB: &str = "pythonforengineers";
+    const COMMENT_LIMIT: i32 = 100;
+
+    reddit
+        .get_recent_comments(SUB, Some(COMMENT_LIMIT), None)
+        .expect("Could not retrieve comments")
+}
 
 fn main() {
-    let toml_str = read_config().expect("No file found");
-    let config: HashMap<String, String> = toml::from_str(&toml_str).expect("Could not deserialize file");
-    println!("{:?}", config);
+    let config = read_config();
+    let reddit = create_app(config);
+
+    let mut comments = get_comments(&reddit);
+    println!("{:?}", comments.next());
 }
