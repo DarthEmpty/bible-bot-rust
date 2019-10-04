66c728ff9e9737ee3c981835f1abdc31142be52e
Joel Clarke <joel.clarke273@gmail.com>
2019-10-04 11:57:15 +0100

    Add test for bucket connection

--- src/s3_access/tests.rs
+++ src/s3_access/tests.rs
@@ -3,6 +3,11 @@
 use super::*;
 
 #[test]
+fn test_connect_to_bucket() {
+    assert!(connect_to_bucket().is_ok());
+}
+
+#[test]
 fn test_load_config() {
     let bucket = connect_to_bucket().unwrap();
     let config = load_config(&bucket);
